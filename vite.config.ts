e3998f546ccf59be8c1667dfd4567b2f0d12e303
fentosv <fentos.dev@gmail.com>
2024-02-29 20:31:50 +0100

    Add coverage script & config

--- vite.config.ts
+++ vite.config.ts
@@ -31,5 +31,9 @@ export default defineConfig({
     // you might want to disable it, if you don't have tests that rely on CSS
     // since parsing CSS is slow
     css: true,
+    coverage: {
+      include: ['src/components'],
+      exclude: ['**/*.stories.ts'],
+    },
   },
 })
