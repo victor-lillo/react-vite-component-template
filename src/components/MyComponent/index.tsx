64c4a57b7c25cbde148e60ac1c8bbb595a8ec8d0
fentosv <fentos.dev@gmail.com>
2024-02-29 20:06:41 +0100

    Update props name

--- src/components/MyComponent/index.tsx
+++ src/components/MyComponent/index.tsx
@@ -1,14 +1,14 @@
 import styles from './styles.module.css'
 import classnames from 'classnames-creator'
 
-interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+interface ComponentProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   primary?: boolean
   size?: 'small' | 'medium' | 'large'
   label: string
   onClick?: () => void
 }
 
-export function MyComponent({ primary = false, size = 'medium', label, ...props }: ButtonProps) {
+export function MyComponent({ primary = false, size = 'medium', label, ...props }: ComponentProps) {
   const style = classnames(styles.button, {
     [styles['button--primary']]: primary,
     [styles[`button--${size}`]]: size,
