2dde74bc65ec0f51e85b9e4340284fd0c8c99ecd
fentosv <fentos.dev@gmail.com>
2024-02-29 18:19:30 +0100

    Implement dts plugin in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts({ exclude: ['**/*.stories.ts'] })],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/main.ts'),
