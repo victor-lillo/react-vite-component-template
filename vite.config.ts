5bbc30922c4a5a3f55b96bfe235461bafddcfa78
fentosv <fentos.dev@gmail.com>
2024-03-10 20:19:38 +0100

    Exclude testing files from type declaration build

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import react from '@vitejs/plugin-react'
 import dts from 'vite-plugin-dts'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), dts({ exclude: ['**/*.stories.ts'] })],
+  plugins: [react(), dts({ exclude: ['**/*.stories.ts', 'src/test', '**/*.test.tsx'] })],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/main.ts'),
