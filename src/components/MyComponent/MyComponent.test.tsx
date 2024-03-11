18c5c8b24300e084150e314e0c9fd310306acf27
fentosv <fentos.dev@gmail.com>
2024-03-11 17:28:42 +0100

    Enhace MyComponent tests

--- src/components/MyComponent/MyComponent.test.tsx
+++ src/components/MyComponent/MyComponent.test.tsx
@@ -1,5 +1,5 @@
-import { cleanup, render, screen } from '@testing-library/react'
-import { afterEach, describe, it } from 'vitest'
+import { cleanup, fireEvent, render, screen } from '@testing-library/react'
+import { afterEach, describe, it, vi } from 'vitest'
 import { MyComponent } from '.'
 
 describe('MyComponent test:', () => {
@@ -23,4 +23,43 @@ describe('MyComponent test:', () => {
     )
     expect(screen.getByRole('button')).toBeDisabled()
   })
+
+  it('onClick triggers properly', async () => {
+    const mockFn = vi.fn()
+
+    render(
+      <MyComponent
+        onClick={mockFn}
+        label='Testing'
+      />
+    )
+
+    expect(mockFn).toHaveBeenCalledTimes(0)
+
+    fireEvent.click(screen.getByRole('button'))
+
+    expect(mockFn).toHaveBeenCalledTimes(1)
+  })
+
+  it('disabled prevents action', async () => {
+    const mockFn = vi.fn()
+
+    render(
+      <MyComponent
+        onClick={mockFn}
+        label='Testing'
+        disabled
+      />
+    )
+
+    expect(mockFn).toHaveBeenCalledTimes(0)
+
+    fireEvent.click(screen.getByRole('button'))
+
+    expect(mockFn).toHaveBeenCalledTimes(0)
+  })
+
+  // https://github.com/vitest-dev/vitest/blob/main/examples/react-testing-lib/src/components/Spinner.test.tsx
+
+  // https://testing-library.com/docs/react-testing-library/example-intro
 })
