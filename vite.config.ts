f42af3d14fd94fe9ffea38a09eaafcf38f731c43
fentosv <fentos.dev@gmail.com>
2024-03-22 17:12:43 +0100

    Fix Vite CSS modules injection with vite-plugin-lib-inject-css

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,45 @@
 /// <reference types="vite/client" />
 /// <reference types="vitest" />
 
+import path from 'node:path'
 import { resolve } from 'path'
+import { fileURLToPath } from 'node:url'
+
+import { globSync } from 'glob'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import dts from 'vite-plugin-dts'
+import { libInjectCss } from 'vite-plugin-lib-inject-css'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), dts({ exclude: ['**/*.stories.ts', 'src/test', '**/*.test.tsx'] })],
+  plugins: [
+    react(),
+    // https://github.com/vitejs/vite/issues/1579#issuecomment-1483756199
+    libInjectCss(),
+    dts({ exclude: ['**/*.stories.ts', 'src/test', '**/*.test.tsx'] }),
+  ],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/main.ts'),
-      name: 'MyLib',
-      fileName: 'my-lib',
+      formats: ['es'],
     },
     rollupOptions: {
       external: ['react', 'react-dom', 'react/jsx-runtime'],
+      // https://rollupjs.org/configuration-options/#input
+      input: Object.fromEntries(
+        globSync(['src/components/**/index.tsx', 'src/main.ts']).map((file) => [
+          // This remove `src/` as well as the file extension from each
+          // file, so e.g. src/nested/foo.js becomes nested/foo
+          path.relative('src', file.slice(0, file.length - path.extname(file).length)),
+          // This expands the relative paths to absolute paths, so e.g.
+          // src/nested/foo becomes /project/src/nested/foo.js
+          fileURLToPath(new URL(file, import.meta.url)),
+        ])
+      ),
       output: {
+        entryFileNames: '[name].js',
+        assetFileNames: 'assets/[name][extname]',
         globals: {
           react: 'React',
           'react-dom': 'React-dom',
