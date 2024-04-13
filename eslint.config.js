595255e94fafa54190761a60ff61dca17c6dc8c9
fentosv <fentos.dev@gmail.com>
2024-04-13 20:24:47 +0200

    Add eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+import eslint from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import eslintConfigPrettier from 'eslint-config-prettier'
+
+// Wait till this is published: https://github.com/storybookjs/eslint-plugin-storybook/pull/152
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintConfigPrettier,
+  {
+    plugins: {
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    },
+    ignorePatterns: ['dist', 'postcss.config.cjs'],
+  }
+)
