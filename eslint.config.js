8f5ed89b5f0a72758e2fcb3ef748d3225be63d3a
fentosv <fentos.dev@gmail.com>
2025-02-22 17:48:06 +0100

    Update ESLint configuration to include React hooks and Storybook support

--- eslint.config.js
+++ eslint.config.js
@@ -1,20 +1,32 @@
-import eslint from '@eslint/js'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
 import eslintConfigPrettier from 'eslint-config-prettier'
+import storybook from 'eslint-plugin-storybook'
 
-// Wait till this is published: https://github.com/storybookjs/eslint-plugin-storybook/pull/156
 export default tseslint.config(
-  eslint.configs.recommended,
-  ...tseslint.configs.recommended,
-  eslintConfigPrettier,
+  { ignores: ['dist', '!.storybook', 'postcss.config.cjs'] },
   {
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...storybook.configs['flat/recommended'],
+      eslintConfigPrettier,
+    ],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
     plugins: {
+      'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
     rules: {
+      ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     },
-    ignorePatterns: ['dist', 'postcss.config.cjs'],
   }
 )
